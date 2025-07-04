f5d3ea06056372ef7edba2a8e3dd55171af11b86
𝐑𝐚𝐟𝐚𝐞𝐥 <rafael.oliveira.silva09@escola.pr.gov.br>
2025-07-04 10:54:53 -0300

    www

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-function inserirNome() {
+function inserirNome( ) {
     let nomeUsuario = prompt("Qual seu nome?");
     let elemento = document.querySelector ("#nome=usuario");
     elemento.textContent = nomeUsuario;
+
 }
 
-inserirNome
+inserirNome( );
